8077b63d585ba17551172da4efff8a675225d42f
Mohamed labbit <151045562+Mohammed0101-lgtm@users.noreply.github.com>
2024-07-16 01:23:43 +0100

    Update input.c

--- input.c
+++ input.c
@@ -7,7 +7,9 @@
 #include <errno.h>
 #include <stdbool.h>
 
+// for reading the command line input
 char *read_line() {
+    // initialize the line input
     char *line = NULL;
 
     line = (char *)malloc(BUFSIZ * sizeof(char) + 1);
@@ -16,7 +18,9 @@ char *read_line() {
         exit(EXIT_FAILURE);
     }
 
+    // read the line input
     if (fgets(line, BUFSIZ, stdin) == NULL) {
+        // incase the stdin is saturated
         if (feof(stdin)) {
             free(line);
             exit(EXIT_SUCCESS);
@@ -27,32 +31,36 @@ char *read_line() {
         }
     }
 
+    // strip newline
     if (line != NULL) {
         size_t len = strlen(line);
         if (len > 0 && line[len - 1] == '\n') 
             line[len - 1] = '\0';
     }
 
-    return line;
+    return line; 
 }
 
+// read commands from a file
 char *read_stream() {
     int i = 0, character;
-    size_t buf_size = BUFSIZ;
     
+    size_t buf_size = BUFSIZ;
     char *line = (char *)malloc(buf_size * sizeof(char));
-
     if (line == NULL) {
         fprintf(stderr, "Memory allocation failed!\n");
         exit(EXIT_FAILURE);
     }
 
+    // while the current character is not 'EOF' or '\n' 
     while ((character = getchar()) != EOF && character != '\n') {
-        line[i] = character;
-        i++;
+        // append the current char to the 'line'
+        line[i++] = character;
 
+        // if 'line' is not enough of a buffer
+        // then double the size to realloc
         if (i >= buf_size) {
-            buf_size *= 2;
+            buf_size *= 2; 
             char *temp = (char *)realloc(line, buf_size * sizeof(char));
 
             if (temp == NULL) {
@@ -69,8 +77,9 @@ char *read_stream() {
     return line;
 }
 
-
+// tokenize the input stream by space seperators
 char **tok_line(char *line) {
+    // buffer to hold tokens
     int buf_size = 64;
     char **tokens = (char **)malloc(buf_size * sizeof(char *));
     if (tokens == NULL) {
@@ -78,28 +87,35 @@ char **tok_line(char *line) {
         exit(EXIT_FAILURE);
     }
 
+    // go token by token
     char *token = NULL;
     int i = 0;
-    token = strtok(line, TOK_DELIM);
+    token = strtok(line, TOK_DELIM); // using strtok that returns token by delimiters
 
+    // loop goes until the end of input stream token by token
     while (token != NULL) {
-        if (token[0] == '#') 
+        if (token[0] == '#')  // ignore comments
             break;
         
-        tokens[i] = token;
-        i++;
+        tokens[i++] = token; // insert token at the end of the array
+
+        // in case the buffer is not sufficient for more tokens
         if (i >= buf_size) {
-            buf_size += buf_size;
+            buf_size += buf_size; // double the buffer size
+            // realloc memory at the same region
             char *temp = (char *)realloc(tokens, buf_size * sizeof(char));
             if (temp == NULL) {
                 fprintf(stderr, "Memory reallocation failed (read_stream)! : %s\n", strerror(errno));
                 free(line);  
                 exit(EXIT_FAILURE);
             }
+
             tokens = &temp;
         }
-        token = strtok(NULL, TOK_DELIM);
+
+        token = strtok(NULL, TOK_DELIM); // go to the next token
     }
-    tokens[i] = NULL;
+
+    tokens[i] = NULL; // null terminate the tokens array
     return tokens;
 }
