0cb19a375c5403bc1130d882279de9fa085ea049
Mohamed labbit <151045562+Mohammed0101-lgtm@users.noreply.github.com>
2024-08-02 16:11:59 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <dirent.h>
 #include <unistd.h>
-#include <errno.h>
-#include <pwd.h>
 
-#include "config.h"
-#include "input.h"
-#include "exec.h"
-#include "command.h"
 #include "shell_inter.h"
 
 
 int main(void) {
-    if (isatty(STDIN_FILENO)) {
+    if (isatty(STDIN_FILENO)) 
         if (shell_inter() != 0) {
-            fprintf(stderr, "Shell interrupted\n");
+            fprintf(stderr, 
+                    "Shell interrupted\n");
             exit(EXIT_FAILURE);
         }
-    } else {
+    else 
         shell_non_inter();
-    }
     
     return 0;
 }
